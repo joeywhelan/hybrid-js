e1d85d1ecb4dc6e87a0a9c9cc09f5c0275e58982
Joey Whelan <joey.whelan@redis.com>
2024-11-24 16:25:19 -0700

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-#SEARCH=redisearch.Linux-ubuntu20.04-x86_64.2.10.7.zip
-
-#if [ ! -f $SEARCH ]
-#then
-#    echo "*** Fetch Search module  ***"
-#    wget -q https://redismodules.s3.amazonaws.com/redisearch/$SEARCH
-#fi 
-
 #!/bin/bash
 if [ ! -d ~/.cache/nim ]
 then
@@ -18,7 +10,6 @@ curl -s -o /dev/null --retry 5 --retry-all-errors --retry-delay 3 -f -k -u "redi
 echo "*** Build Cluster ***"
 docker exec -it re1 /opt/redislabs/bin/rladmin cluster create name cluster.local username redis@redis.com password redis
 sleep 1
-#echo "*** Load Modules ***"
-#curl -k -u "redis@redis.com:redis" https://localhost:9443/v1/modules -F module=@$SEARCH
+
 echo "*** Build Target Redis DB ***"
 curl -s -o /dev/null -k -u "redis@redis.com:redis" https://localhost:9443/v1/bdbs -H "Content-Type:application/json" -d @redb.json
